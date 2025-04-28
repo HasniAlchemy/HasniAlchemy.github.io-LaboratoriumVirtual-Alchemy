68f597d4999dc9b14b62b65740cf7712bcfa80a9
HasniAlchemy <hasni.kesuma26@guru.sma.belajar.id>
2025-04-28 15:30:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-// Tampilkan pesan sambutan ketika halaman selesai dimuat
-document.addEventListener('DOMContentLoaded', function() {
-    alert('Selamat datang di Laboratorium Virtual Alchemy!');
-});
 
-// Tambahkan fungsi klik tombol untuk menampilkan info tambahan
-function tampilkanInfo() {
-    const infoDiv = document.getElementById('info-tambahan');
-    if (infoDiv.style.display === 'none') {
-        infoDiv.style.display = 'block';
-    } else {
-        infoDiv.style.display = 'none';
-    }
-}
